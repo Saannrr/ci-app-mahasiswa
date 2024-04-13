23e4249e1d64e1d8417e8013902380354fea4449
saannrr <ihsan.ican2002@gmail.com>
2024-04-13 23:01:27 +0700

    fix validation errors jadi lebih bagus

--- application/views/mahasiswa/tambah.php
+++ application/views/mahasiswa/tambah.php
@@ -6,23 +6,33 @@
                     Form Tambah Data Mahasiswa
                 </div>
                 <div class="card-body">
-                    <?php if (validation_errors()) : ?>
-                        <div class="alert alert-danger" role="alert">
-                            <?= validation_errors(); ?>
-                        </div>
-                    <?php endif; ?>
                     <form action="" method="post">
                         <div class="form-group">
                             <label for="nama">Nama</label>
                             <input type="text" class="form-control" id="nama" name="nama" placeholder="nama">
+                            <?php if (form_error('nama')) : ?>
+                                <small class="text-danger">
+                                    <?= form_error('nama'); ?>
+                                </small>
+                            <?php endif; ?>
                         </div>
                         <div class="form-group">
                             <label for="nrp">NRP</label>
                             <input type="text" class="form-control" id="nrp" name="nrp" placeholder="nrp">
+                            <?php if (form_error('nrp')) : ?>
+                                <small class="text-danger">
+                                    <?= form_error('nrp'); ?>
+                                </small>
+                            <?php endif; ?>
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>
                             <input type="text" class="form-control" id="email" name="email" placeholder="email">
+                            <?php if (form_error('email')) : ?>
+                                <small class="text-danger">
+                                    <?= form_error('email'); ?>
+                                </small>
+                            <?php endif; ?>
                         </div>
                         <div class="form-group">
                             <label for="jurusan">Jurusan</label>
@@ -34,6 +44,11 @@
                                 <option value="Teknik Seni">Teknik Seni</option>
                                 <option value="Sastra Mesin">Sastra Mesin</option>
                             </select>
+                            <?php if (form_error('jurusan')) : ?>
+                                <small class="text-danger">
+                                    <?= form_error('jurusan'); ?>
+                                </small>
+                            <?php endif; ?>
                         </div>
                         <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
                         <a href="<?= base_url(); ?>mahasiswa" class="btn btn-secondary float-left">Kembali</a>
